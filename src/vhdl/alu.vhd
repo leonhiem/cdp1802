f7ac963db341af7404e5125743879d885a3cb0a9
Leon Hiemstra <hiemstra@astron.nl>
2021-03-31 00:19:06 +0200

    finished ADD instruction, enhanced alu

--- src/vhdl/alu.vhd
+++ src/vhdl/alu.vhd
@@ -30,44 +30,37 @@ BEGIN
   p_alu : PROCESS(oper, alu_in, d_in, carry_in)
   BEGIN
 
-    carry_out <= '0';
     d_out <= (OTHERS => 'Z');
 
     CASE oper IS
       -- logic operations:
       WHEN c_ALU_OR =>
-        alu_out <= d_in OR alu_in;
+        tmp <= "0" & (d_in OR alu_in);
       WHEN c_ALU_XOR =>
-        alu_out <= d_in XOR alu_in;
+        tmp <= "0" & (d_in XOR alu_in);
       WHEN c_ALU_AND =>
-        alu_out <= d_in AND alu_in;
+        tmp <= "0" & (d_in AND alu_in);
       WHEN c_ALU_SHR => -- >>
-        alu_out <= "0" & alu_in(7 DOWNTO 1);
-        carry_out <= alu_in(0);
+        tmp <= alu_in(0) & "0" & alu_in(7 DOWNTO 1);
       WHEN c_ALU_SHL => -- <<
-        alu_out <= alu_in(6 DOWNTO 0) & "0";
-        carry_out <= alu_in(7);
+        tmp <= alu_in & "0";
       WHEN c_ALU_RSHR => -- >>
-        alu_out <= alu_in(0) & alu_in(7 DOWNTO 1);
-        carry_out <= alu_in(0);
+        tmp <= alu_in(0) &  alu_in(0) & alu_in(7 DOWNTO 1);
       WHEN c_ALU_RSHL => -- <<
-        alu_out <= alu_in(6 DOWNTO 0) & alu_in(7);
-        carry_out <= alu_in(7);
+        tmp <= alu_in & alu_in(7);
 
       -- arithmetic operations:
       WHEN c_ALU_U_ADD => -- unsigned addition
-        --tmp <= "0" & std_logic_vector(unsigned(d_in) + unsigned(alu_in));
-        --alu_out <= tmp(7 DOWNTO 0);
-        --tmp <= "0" & std_logic_vector(unsigned(d_in) + unsigned(alu_in));
-        --carry_out <= tmp(8);
-
-        alu_out <= std_logic_vector(unsigned(d_in) + unsigned(alu_in));
+        tmp <= std_logic_vector(unsigned('0' & d_in) + unsigned('0' & alu_in));
 
       WHEN OTHERS => -- c_ALU_NOP
-        alu_out <= d_in;
+        tmp <= "0" & d_in;
         d_out <= alu_in;
     END CASE;
 
   END PROCESS;
+
+  alu_out   <= tmp(7 DOWNTO 0);
+  carry_out <= tmp(8);
 
 END str;
